f7c58122fafee90bb8ca51decf1ed745964fdc53
Vladislav <vladokatr@gmail.com>
2020-02-09 23:13:21 +0200

    added upload file handling

--- src/db/init_db.sql
+++ src/db/init_db.sql
@@ -7,3 +7,12 @@ id INTEGER AUTO_INCREMENT,
 name TEXT,
 PRIMARY KEY (id)
 );
+
+CREATE TABLE if not exists images
+(
+id INTEGER AUTO_INCREMENT,
+file_name TEXT,
+uploaded_on DATE,
+
+PRIMARY KEY (id)
+);
